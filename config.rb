2078f3d7ccabc811894980b536bdbc5fbe7d0d6a
Andrew Larkin <andrewlarkin2@gmail.com>
2013-01-27 01:30:03 -0500

    Removing webapp files

--- config.rb
+++
@@ -1,2 +0,0 @@
-sass_dir = 'webapp/public/stylesheets/scss'
-css_dir = 'webapp/public/stylesheets/css'
--- webapp/public/javascripts/requirejs_config.js
+++
@@ -1,7 +0,0 @@
-requirejs.config({
-  baseUrl: "js",
-  paths: {
-    jquery: 'vendor/jquery-1.8.3',
-    io: '../socket.io/socket.io'
-  }
-});
